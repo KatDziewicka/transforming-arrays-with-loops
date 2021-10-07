fe56f042ea7a432dd503dfc938e936adc95f5180
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-10-07 20:20:03 +0200

    1d partially solved

---
+++ 1e-number-and-string.js
@@ -0,0 +1,6 @@
+// **Challenge:** Write a function that takes an array of numbers and returns an array of plain objects, with properties:
+
+// - `asNumber` - the original number
+// - `asString` - a string representation of the number
+
+// **Example:** `[4, -3.2] -> [{ asNumber: 4, asString: '4' }, { asNumber: -3.2, asString: '-3.2' }]`
