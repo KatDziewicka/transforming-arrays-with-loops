cb8aca8e571372fca9e02fb07c3ebc06b90855d1
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-10-07 18:24:48 +0200

    Pseudocode function signature

---
+++ 0-lowercase-domains.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of strings representing domain names, 
+// in mixed case, and returns an array of the domain names in all-lowercase.
+
+// function name: lowerCaseDomain
+
+// parameter:
+//  - domainNames - array of strings 
+
+// returns: array of strings
