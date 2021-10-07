0e5fce62040d573a9d0586abeef1d288bc8a9a5b
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-10-07 18:29:27 +0200

    Test cases and pseudocode body

--- 0-lowercase-domains.js
+++ 0-lowercase-domains.js
@@ -1,9 +1,31 @@
 // Write a function that takes an array of strings representing domain names, 
 // in mixed case, and returns an array of the domain names in all-lowercase.
 
-// function name: lowerCaseDomain
+// function name: lowerCaseDomains
 
 // parameter:
 //  - domainNames - array of strings 
 
 // returns: array of strings
+
+// pseudocode:
+// ===========
+// assign allLowerCase as an empty array 
+// assign lowerCaseDom as an empty string
+
+// iterate through each element of domainNames
+//     reassign element.toLowerCase() to lowerCaseDom
+//     ass lowerCaseDom to the end of allLowerCase
+
+// return allLowerCase
+
+// test cases
+
+// console.log(
+// 	lowerCaseDomains(["academy.tech", "GITHUB.COM", "StackOverflow.com", "mOzIlLa.OrG"]),
+// 	["academy.tech", "github.com", "stackoverflow.com", "mozilla.org"]
+// )
+
+// console.log(
+// 	lowerCaseDomains(["m1a.tech", "example.cO.UK", "HTTPS://JavaScript.Info"]),
+// 	["m1a.tech", "example.co.uk", "https://javascript.info"]
